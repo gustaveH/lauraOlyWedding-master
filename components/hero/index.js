bbec8b3d4fcf9757af484f170b24b61e095b5302
Gustave Habimana <34315341+gustaveH@users.noreply.github.com>
2023-11-05 15:59:27 -0500

    Update index.js
    edited the hero page

--- components/hero/index.js
+++ components/hero/index.js
@@ -17,7 +17,7 @@ const Hero = (props) => {
                                         <h2>Olivier <span>&</span> Laura</h2>
                                     </div>
                                     <div data-swiper-parallax="400" className="slide-text">
-                                        <p>We Are Getting Married!</p>
+                                        <p>We Are Getting Married</p>
                                     </div>
                                     <div className="wpo-wedding-date">
                                         <div className="clock-grids">
