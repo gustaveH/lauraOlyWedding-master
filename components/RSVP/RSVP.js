ad0bc25d1b50733e1a58941fb9a973d3ac536cd1
Gustave Habimana <34315341+gustaveH@users.noreply.github.com>
2023-11-06 16:12:49 -0500

    contact form

--- components/RSVP/RSVP.js
+++ components/RSVP/RSVP.js
@@ -42,7 +42,7 @@ export const RSVP = (props) => {
                     <div className="wpo-contact-form-area">
                         <SectionTitle MainTitle={'Are you attending?'} />
 
-                        <form onSubmit={(e) => submitHandler(e)} className="contact-validation-active">
+                        <form onSubmit={(e) => submitHandler(e)} className="contact-validation-active" method="POST" data-netlify="true">
                             <div className="form-field">
                                 <input
                                     value={forms.name}
