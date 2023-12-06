660bcdaa87ae344c730b81014c7555d2786f251e
Gustave Habimana <34315341+gustaveH@users.noreply.github.com>
2023-12-06 18:19:04 -0500

    ESVP option edit

--- components/RSVP/index.js
+++ components/RSVP/index.js
@@ -43,23 +43,27 @@ const RSVP = (props) => {
                             </div>
                             <div className="form-field">
                                 <p>Will you be attending?</p>
-                                <input
-                                    id="Response"
-                                    type="text"
-                                    name="Response"
-                                    className="form-control"
-                                    placeholder="Yes/No" />
-                                <ValidationError prefix="Your response" field="Yes/No" errors={state.errors} />
+                                <select className="form-control" name="attending" id="attending" required="">
+                                    <option value="" selected="" disabled="">Select</option>
+                                    <option value="Yes">Yes</option>
+                                    <option value="No">No</option>
+                                </select>
                             </div>
                             <div className="form-field">
-                                <p>Please list names of the party attending.</p>
-                                <input
-                                    id="Attendies"
-                                    type="text"
-                                    name="Attendies"
-                                    className="form-control"
-                                    placeholder="John Doe, Jane Doe" />
-                                <ValidationError prefix="Your response" field="Guests" errors={state.errors} />
+                                <p>Number of people attending.</p>
+                                <select className="form-control" name="People Attenting" id="plus" required="">
+                                    <option value="0" selected="">0</option>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                    <option value="6">6</option>
+                                    <option value="7">7</option>
+                                    <option value="8">8</option>
+                                    <option value="9">9</option>
+                                    <option value="10">10</option>
+                                </select>
                             </div>
                             <div className="submit-area">
                                 <button type="submit" className="theme-btn">Submit</button>
