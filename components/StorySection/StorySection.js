060d6cde15039fd044091b45893d73a619f2b1fa
Gustave Habimana <34315341+gustaveH@users.noreply.github.com>
2023-12-08 11:21:46 -0500

    wedding to Ceremony

--- components/StorySection/StorySection.js
+++ components/StorySection/StorySection.js
@@ -15,7 +15,7 @@ const StorySection = (props) => {
         <section className="story-section section-padding">
             <div>
                 <h1>A Love Story</h1>
-                <p className='ourLoveStory'>Welcome to our celebration of love that transcends distance! Our love story began with a connection, sparked by a beloved family friend and Olivier’s aunt. Little did we know, she saw the potential that would blossom into a deep bond and everlasting love. As we prepare to embark on this journey, we invite you to join us in celebrating our traditional wedding, a harmonious celebration of Rwandan tradition. As we unite our lives in our tapestry of cultures and shared love orchestrated by God, will look forward to your presence.
+                <p className='ourLoveStory'>Welcome to our celebration of love that transcends distance! Our love story began with a connection, sparked by a beloved family friend and Olivier’s aunt. Little did we know, she saw the potential that would blossom into a deep bond and everlasting love. As we prepare to embark on this journey, we invite you to join us in celebrating our traditional ceremony, a harmonious celebration of Rwandan tradition. As we unite our lives in our tapestry of cultures and shared love orchestrated by God, will look forward to your presence.
                 </p>
                 <h4> Oli & Uza</h4>
             </div>
